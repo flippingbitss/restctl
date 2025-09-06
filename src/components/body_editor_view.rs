7a58ad7ec961fdbef330ebb0be2fb7ea49949fdd
flippingbitss <matharumanpreet00@gmail.com>
2025-09-06 13:20:04 -0400

    body_editor: add syntax highlight and scroll view

--- src/components/body_editor_view.rs
+++ src/components/body_editor_view.rs
@@ -1,7 +1,27 @@
 pub fn show(ui: &mut egui::Ui, body: &mut String) {
-    let editor = egui::TextEdit::multiline(body)
-        .code_editor()
-        .desired_width(ui.available_width())
-        .desired_rows(20);
-    ui.add(editor);
+    let mut theme = egui_extras::syntax_highlighting::CodeTheme::from_memory(ui.ctx(), ui.style());
+    let mut layouter = |ui: &egui::Ui, buf: &dyn egui::TextBuffer, wrap_width: f32| {
+        let mut layout_job = egui_extras::syntax_highlighting::highlight(
+            ui.ctx(),
+            ui.style(),
+            &theme,
+            buf.as_str(),
+            "json",
+        );
+        layout_job.wrap.max_width = wrap_width;
+        ui.fonts(|f| f.layout_job(layout_job))
+    };
+
+    egui::ScrollArea::vertical()
+        .min_scrolled_height(ui.available_height())
+        .max_height(ui.available_height())
+        .show(ui, |ui| {
+            ui.add(
+                egui::TextEdit::multiline(body)
+                    .code_editor()
+                    .desired_rows(20)
+                    .desired_width(f32::INFINITY)
+                    .layouter(&mut layouter),
+            );
+        });
 }
